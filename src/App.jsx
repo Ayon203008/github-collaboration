61afef0d6c9bc713dff68e24e7c35335ec5290b5
Ayon203008 <paypal572874@gmail.com>
2025-09-16 15:00:33 +0600

    new react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './Hero';
+
+const App = () => {
+  return (
+    <div>
+      <h1> Hello world </h1>
+      <Hero/>
+    </div>
+  );
+};
+
+export default App;
